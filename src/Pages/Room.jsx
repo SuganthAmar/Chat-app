23c437ae75136fd5e5b1955d84d2e4ff9d1316f7
SuganthAmar <asuganth2003@gmail.com>
2024-02-18 15:49:17 +0530

    Baas Connection Made

--- src/Pages/Room.jsx
+++ src/Pages/Room.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
+import { COLLECTION_ID_MESSAGES, DATABASE_ID, databases } from '../appwriteConfig'
 
 const Room = () => {
+    useEffect(()=>{
+        getMessage();
+    },[])
+    const getMessage =async()=>{
+        const response=await databases.listDocuments(DATABASE_ID,COLLECTION_ID_MESSAGES);
+        console.log(response);
+    } 
+
+
   return (
     <div>Room</div>
   )
--- src/appwriteConfig.js
+++ src/appwriteConfig.js
@@ -1,7 +1,14 @@
-import { Client } from 'appwrite';
+import { Client,Databases } from 'appwrite';
 
 const client = new Client();
 
+
+export const PROJECT_ID='65d1cd6b5aa5d64c685c';
+export const DATABASE_ID='65d1d0996e57d5392274';
+export const COLLECTION_ID_MESSAGES='65d1d0ad89facef670e4';
 client
-    .setEndpoint(process.env.APPWRITE_ENDPOINT)
-    .setProject(process.env.APPWRITE_PROJECT_ID);
+.setEndpoint('https://cloud.appwrite.io/v1')
+.setProject('65d1cd6b5aa5d64c685c');
+
+export const databases = new Databases(client);
+export default client;
